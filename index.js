1a7aabccedd4ff46ac1362818740a432b50cc295
korayoksuztepe <korayoksuztepe@gmail.com>
2015-04-04 02:28:25 +0300

    verse link adder

--- index.js
+++ index.js
@@ -323,9 +323,9 @@ angular.module('ionicApp', ['ngResource', 'ngRoute', 'facebook', 'restangular', 
                     var dataLength = data.length;
                     for (index = 0; index < dataLength; ++index) {
                         //add verse links
-                        data[index]=data[index].replace("27:30", "<a href='javascript: alert(\"oldu\");'>___27:30___</a>");
+                        dataContent=data[index].replace("27:30", "<a href='javascript: alert(\"oldu\");'>___27:30___</a>");
 
-                        html += "<div class='row'><div class='col-xs-1 footnote_bullet'>&#149;</div><div class='col-xs-11'>" + data[index] + "</div></div>";
+                        html += "<div class='row'><div class='col-xs-1 footnote_bullet'>&#149;</div><div class='col-xs-11'>" + dataContent + "</div></div>";
                     }
                     html += '</div>';
                     footnoteDivElement.innerHTML = footnoteDivElement.innerHTML + html;
