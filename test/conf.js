b17c2747911fe7a70e62c9c0174617175fe062be
sodepro <esprogsoft@hotmail.com>
2015-05-03 22:32:16 +0300

    0105

--- test/conf.js
+++ test/conf.js
@@ -6,6 +6,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
         'spec1.js',
+        'temiz.js',
         'spec2.js',
         'spec3.js',
         'spec4.js',
---
+++ test/temiz.js
@@ -0,0 +1,9 @@
+describe('ceviri gosterimi', function() {
+	 
+	  it('Not ekleme', function() {
+	
+		  browser.get('https://securewebserver.net/jetty/qttest/rest/users/testdelete');
+	      
+	  });
+	  	  
+});
