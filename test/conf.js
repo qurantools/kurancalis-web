b67aec180c43864c35ccc745b3dfd412c64e2b56
sodepro <esprogsoft@hotmail.com>
2015-05-06 01:07:24 +0300

    0605

--- test/conf.js
+++ test/conf.js
@@ -14,7 +14,8 @@ exports.config = {
         'spec5.js',
         'spec6.js',
         'spec7.js',
-        'spec8.js'
+        'spec8.js',
+        'spec9.js'
     ],
     jasmineNodeOpts: {defaultTimeoutInterval: 360000},
     multiCapabilities: [
