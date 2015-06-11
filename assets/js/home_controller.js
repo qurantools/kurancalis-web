8f97dee1b80341f3e57b44054d709937e3cddb71
korayoksuztepe <korayoksuztepe@gmail.com>
2015-06-11 11:59:49 +0300

    tags have been included to annotation filter

--- assets/js/home_controller.js
+++ assets/js/home_controller.js
@@ -1,7 +1,7 @@
 angular.module('ionicApp')
     .controller('HomeCtrl', function ($scope, $q, $routeParams, $location, $timeout, ListAuthors, ChapterVerses, User, Footnotes, Facebook, Restangular, localStorageService, $document, $filter, $rootScope, $state, $stateParams, $ionicModal, $ionicScrollDelegate, $ionicPosition, authorization) {
         console.log("HomeCtrl");
-        $scope.currentPage=$scope.getCurrentPage();
+        $scope.currentPage = $scope.getCurrentPage();
 
         $scope.list_translations();
 
@@ -18,17 +18,19 @@ angular.module('ionicApp')
 
 
         $scope.annotationTextSearch = function (item) {
-//TODO: filtre çalışmıyor
-            if(config_data.isMobile){
-                if(document.getElementById("searchText") && document.getElementById("searchText").value){
+//TODO: filtre mobilde çalışmıyor
+            if (config_data.isMobile) {
+                if (document.getElementById("searchText") && document.getElementById("searchText").value) {
                     var searchText = document.getElementById("searchText").value.toLowerCase();
-                    console.log("searchText"+searchText)
-                    $scope.searchText=searchText;
+                    console.log("searchText" + searchText)
+                    $scope.searchText = searchText;
                 }
-            }else{
+            } else {
                 var searchText = $scope.searchText.toLowerCase();
             }
-            if (item.quote.toLowerCase().indexOf(searchText) > -1 || item.text.toLowerCase().indexOf(searchText) > -1) {
+            var tags = '';
+            if (typeof item.tags[0] != 'undefined')tags = item.tags[0].toLowerCase();
+            if (item.quote.toLowerCase().indexOf(searchText) > -1 || item.text.toLowerCase().indexOf(searchText) > -1 || tags.indexOf(searchText) > -1) {
                 console.log("true")
                 return true;
             } else {
