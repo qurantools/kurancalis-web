ed7a38eb71a7fcda85d80a5f72d81902a9e04f16
kurtuluso <kurtulus@posta.la>
2015-12-02 23:43:38 +0200

    editor modal ekraninin erken kapanmasindan dolayi kayit yapilamiyordu, duzeltildi.

--- assets/js/home_controller.js
+++ assets/js/home_controller.js
@@ -385,7 +385,10 @@ angular.module('ionicApp')
                     $scope.closeModal('editor');
                 }
 
+                $('#annotationModal').modal('hide');
+
                 $scope.hideProgress("submitEditor");
+
 
             },350);
 
