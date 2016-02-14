12472b888de8856345b0303e9452ae06eca082a6
kurtuluso <kurtulus@posta.la>
2016-02-14 23:06:33 +0200

    static view for inference added to integrate facebook share

---
+++ assets/php/static_view.php
@@ -0,0 +1,30 @@
+<?
+
+$SITE_ROOT = "https://securewebserver.net/jetty/qttest/rest/";
+
+$jsonData = getData($SITE_ROOT);
+makePage($jsonData, $SITE_ROOT);
+
+
+function getData($siteRoot) {
+    $id = ctype_digit($_GET['id']) ? $_GET['id'] : 1;
+    $rawData = file_get_contents($siteRoot.'inferences/'.$id);
+    return json_decode($rawData);
+}
+
+function makePage($data, $siteRoot) {
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta property="og:title" content="<?php echo $data->title; ?>" />
+        <meta property="og:description" content="<?php echo $data->content; ?>" />
+        <meta property="og:image" content="<?php echo $data->image; ?>" />
+    </head>
+    <body>
+        <p><?php echo $data->content; ?></p>
+        <img src="<?php echo $data->image; ?>">
+    </body>
+    </html>
+<?php
+}
