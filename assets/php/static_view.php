126a8403f5b359712a416e9fe09c63c2ec02007b
kurtuluso <kurtulus@posta.la>
2016-02-15 23:38:51 +0200

    Image manager url fixed

--- assets/php/static_view.php
+++ assets/php/static_view.php
@@ -18,7 +18,7 @@ function makePage($data, $siteRoot) {
     <html>
     <head>
         <meta property="fb:app_id" content="295857580594128" />
-        <meta property="og:url"    content="http://test.kurancalis.com/__/inference/display/<?php echo $data->id; ?>" />
+        <meta property="og:url"    content="http://kurancalis.com/__/inference/display/<?php echo $data->id; ?>" />
         <meta property="og:title" content="<?php echo $data->title; ?>" />
         <meta property="og:description" content="<?php echo $data->brief; ?>" />
         <meta property="og:image" content="<?php echo $data->image; ?>" />
