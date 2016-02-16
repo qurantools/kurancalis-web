96b8a508870bbbab981859d46ca2085122174a07
Mehmet Güngören <mehmet_gungoren@hotmail.com>
2016-02-17 01:04:54 +0200

    KCS-835: Mobile Yardım menüsü auto play video eklendi.

--- app/components/help/help_controller.js
+++ app/components/help/help_controller.js
@@ -1,19 +1,61 @@
 angular.module('ionicApp')
     .controller('HelpController', function ($scope, $routeParams, $location, $timeout, authorization,
-                                                     localStorageService, Restangular, $ionicModal) {
+                                                     localStorageService, Restangular, $ionicModal, $sce) {
         $scope.title = "Yardım";
 
         $scope.menuList=[
-            {id:1, title:"Intro", src:""},
-            {id:2, title:"Not Paylaşma", src:""},
-            {id:3, title:"Ayraç Kullanma", src:""},
-            {id:4, title:"Etiket Kullanımı", src:""},
-            {id:5, title:"Not Arama", src:""}];
+            {   name : "Intro",
+                submenu : [
+                    {id:11, title:"Intro-1", source:[{src: $sce.trustAsResourceUrl("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"), type: "video/mp4"}]},
+                    {id:12, title:"Intro-2", source:[{src: $sce.trustAsResourceUrl("https://youtu.be/uqROdKnNfQk"), type: "video/mp4"}]},
+                    {id:13, title:"Intro-3", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:14, title:"Intro-4", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:15, title:"Intro-5", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]}
+                ]
+            },
+            {   name : "Not Paylaşma",
+                submenu : [
+                    {id:21, title:"Not Paylaşma-1", source:[{src: $sce.trustAsResourceUrl("http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4"), type: "video/mp4"}]},
+                    {id:22, title:"Not Paylaşma-2", source:[{src: $sce.trustAsResourceUrl("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"), type: "video/mp4"}]},
+                    {id:23, title:"Not Paylaşma-3", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:24, title:"Not Paylaşma-4", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:25, title:"Not Paylaşma-5", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]}
+                ]
+            },
+            {   name : "Ayraç Kullanma",
+                submenu : [
+                    {id:31, title:"Ayraç Kullanma-1", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:32, title:"Ayraç Kullanma-2", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:33, title:"Ayraç Kullanma-3", source:[{src: $sce.trustAsResourceUrl("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"), type: "video/mp4"}]},
+                    {id:34, title:"Ayraç Kullanma-4", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:35, title:"Ayraç Kullanma-5", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]}
+                ]
+            },
+            {   name : "Etiket Kullanımı",
+                submenu : [
+                    {id:41, title:"Etiket Kullanımı-1", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:42, title:"Etiket Kullanımı-2", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:43, title:"Etiket Kullanımı-3", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:44, title:"Etiket Kullanımı-4", source:[{src: $sce.trustAsResourceUrl("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"), type: "video/mp4"}]},
+                    {id:45, title:"Etiket Kullanımı-5", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]}
+                ]
+            },
+            {   name : "Not Arama",
+                submenu : [
+                    {id:51, title:"Not Arama-1", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:52, title:"Not Arama-2", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:53, title:"Not Arama-3", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:54, title:"Not Arama-4", source:[{src: $sce.trustAsResourceUrl(""), type: "video/mp4"}]},
+                    {id:55, title:"Not Arama-5", source:[{src: $sce.trustAsResourceUrl("http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"), type: "video/mp4"}]}
+                ]
+            }];
 
         $scope.selectedIndex = 0;
+        $scope.selectedMenu = 0;
 
         $scope.runHelpModal = function(index){
-           $scope.selectedIndex = index;
+           $scope.selectedMenu = index;
+           $scope.selectedIndex = 0;
            $scope.help_modal.show();
         }
 
@@ -40,33 +82,23 @@ angular.module('ionicApp')
             }
          }
 
-         $scope.nextTutorial = function(){
+        $scope.nextTutorial = function(){
             $scope.selectedIndex++;
-            if ($scope.selectedIndex >= $scope.menuList.length){
+            if ($scope.selectedIndex >= $scope.menuList[$scope.selectedMenu].submenu.length){
                 $scope.help_modal.hide();
             }
-         }
+        }
 
-        /*this.config = {
-            sources: [
-                /*{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-            ],
-            tracks: [
-                {
-                    src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                    kind: "subtitles",
-                    srclang: "en",
-                    label: "English",
-                    default: ""
-                }
-            ],
+        $scope.onError = function(event){
+            alert(event);
+        }
+
+        this.config = {
             theme: "../../../assets/lib/videogular/videogular.css",
             plugins: {
                 poster: "http://www.videogular.com/assets/images/videogular.png"
             }
-        }*/
+        }
 
          $scope.initHelpParameters();
     });
