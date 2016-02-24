92d5c22a8b611821cb21031e38b4e0cec71b4a43
Mehmet Gungoren <mgungoren@takasdom>
2016-02-24 11:30:37 +0200

    KCS-889: Mobil yanlış link paylasimi [payalşılan linklerde m/www olmayacak ve mobilden yapılan paylaşımlarda # yerine __ olacak şekilde güncelleme yapıldı.]

--- app/components/inferences/inference_display_controller.js
+++ app/components/inferences/inference_display_controller.js
@@ -228,7 +228,7 @@ angular.module('ionicApp')
                 $scope.inference_info(inferenceId);
             });
             if(config_data.isMobile){
-                $scope.shareUrl =  config_data.webAddress + "/#/inference/display/" + $scope.inferenceId;
+                $scope.shareUrl =  config_data.webAddress + "/__/inference/display/" + $scope.inferenceId;
             }else{
                 $scope.shareUrl =  $location.absUrl().split('#')[0] + "#/inference/display/" + $scope.inferenceId;
             }
