3bf641fa78e6edde05ffab0706dc88eed178d61b
kurtuluso <kurtulus@posta.la>
2016-03-20 23:50:05 +0200

    inference display errors fixed, help videos added for intro

--- create_sqlite_from_mysql.sh
+++ create_sqlite_from_mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mysql2sqlite.sh --default-character-set=UTF8 -u kurancalis -p"$1" kurancalis chapter verse footnote author translation | ./sqlite3 kurancalis.db
+./mysql2sqlite.sh --default-character-set=UTF8 -u $1 -p"$2" kurancalis chapter verse footnote author translation | ./sqlite3 kurancalis.db
